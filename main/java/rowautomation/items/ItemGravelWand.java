3b611c4b8757648b62f8da2f6a373a85137342d6
DonBruce64 <rapscallion827@gmail.com>
2015-12-12 01:09:51 -0600

    Added gravel wand.

---
+++ main/java/rowautomation/items/ItemGravelWand.java
@@ -0,0 +1,37 @@
+package rowautomation.items;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+import rowautomation.ROWAM;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class ItemGravelWand extends Item{
+	public ItemGravelWand(){
+		super();
+		this.setCreativeTab(ROWAM.tabROWAM);
+		this.setUnlocalizedName("GravelWand");
+	}
+	
+	@Override
+	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player){
+		for(int i=0; i<world.loadedTileEntityList.size(); ++i){
+			TileEntity tile = (TileEntity) world.loadedTileEntityList.get(i);
+			if(tile.getClass().getName().contains("net.row.tileentity.TileEntity")){
+				world.setBlock(tile.xCoord, tile.yCoord - 1 , tile.zCoord, Blocks.grass);
+			}
+		}
+		return stack;
+	}
+	
+	 @Override
+	 @SideOnly(Side.CLIENT)
+	 public void registerIcons(IIconRegister register){
+		 itemIcon = register.registerIcon("rowam:gravelwand");
+	 }
+}
